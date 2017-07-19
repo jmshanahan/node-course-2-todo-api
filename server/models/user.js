4ae96cebbc3027d756a5de4409cd73aaae1029d5
jmshanahan <comeraghsolutions@gmail.com>
2017-07-19 16:51:09 +0100

    Add post todos and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var userSchema = new Schema({
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        minlength: 1
+    }
+});
+
+ var User = mongoose.model('User',userSchema);

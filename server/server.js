5222e4666aa18f06017cedd1e616a261393c91f9
jmshanahan <comeraghsolutions@gmail.com>
2017-08-17 12:15:58 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -103,8 +103,21 @@ app.post('/users', (req, res) => {
 });
 
 
-app.get('/users/me',authenticate,(req,res) => {
+app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+        res.send(user);
+    }).catch((e) => {
+        res.status(400).send();
+    });
+
 });
 
 app.listen(port, () => {
